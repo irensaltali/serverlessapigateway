a766368af56d7d27600d4002ed35ac92554f1a98
İren SALTALI <iren@saltali.com>
2024-01-21 00:23:26 +0300

    Add path-ops module for path matching

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -1,19 +1,17 @@
 import apiConfig from './api-config.json';
+import './path-ops';
+import { pathsMatch } from './path-ops';
 
-// Export a default object containing event handlers
+
 export default {
-	// The fetch handler is invoked when this worker receives a HTTP(S) request
-	// and should return a Response (optionally wrapped in a Promise)
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		// You'll find it helpful to parse the request.url string into a URL object. Learn more at https://developer.mozilla.org/en-US/docs/Web/API/URL
 		const url = new URL(request.url);
 
-		var matchedPath = apiConfig.paths.find((item) => item.path === url.pathname && item.method === request.method);
+		var matchedPath = apiConfig.paths.find((item) => pathsMatch(item.path, url.pathname) && item.method === request.method);
 		console.log(matchedPath);
 		if (matchedPath) {
-			console.log(matchedPath);
 			if (matchedPath.integration) {
-				console.log('type:'+matchedPath.integration.type);
+				console.log('type:' + matchedPath.integration.type);
 				if (matchedPath.integration.type === 'http_proxy') {
 					const server = apiConfig.servers.find((server) => server.alias === matchedPath?.integration?.server);
 					console.log(server);
