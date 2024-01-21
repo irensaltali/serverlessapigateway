493121e864f2ffcf0335f3131fb91718d08a70cb
İren SALTALI <iren@saltali.com>
2024-01-21 13:07:09 +0300

    Handle OPTIONS requests

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -1,5 +1,6 @@
 import apiConfig from './api-config.json';
 import { pathsMatch } from './path-ops';
+import { setCorsHeaders } from "./cors";
 
 
 export default {
@@ -7,34 +8,31 @@ export default {
 		const url = new URL(request.url);
 
 		// Filter paths based on URL match
-		var matchedPaths = apiConfig.paths.filter(item => pathsMatch(item.path, url.pathname));
+		const matchedPaths = apiConfig.paths.filter(item => pathsMatch(item.path, url.pathname));
 
-		// First, try to find an exact method match
-		var exactMethodMatch = matchedPaths.find(item => item.method === request.method);
-		if (exactMethodMatch) 
-			var matchedPath = exactMethodMatch;
-		else
-			// If no exact match, fallback to ANY method
-			var matchedPath = matchedPaths.find(item => item.method === 'ANY');
-		
-		console.log(matchedPath);
+		// Find the matched path
+		const matchedPath = matchedPaths.find(item => item.method === request.method) || matchedPaths.find(item => item.method === 'ANY');
+
 		if (matchedPath) {
-			if (matchedPath.integration) {
-				console.log('type:' + matchedPath.integration.type);
-				if (matchedPath.integration.type === 'http_proxy') {
-					const server = apiConfig.servers.find((server) => server.alias === matchedPath?.integration?.server);
-					console.log(server);
-					const modifiedRequest = new Request(server?.url + url.pathname, request);
-					return fetch(modifiedRequest);
+			if (matchedPath.integration && matchedPath.integration.type === 'http_proxy') {
+				const server = apiConfig.servers.find(server => server.alias === matchedPath.integration.server);
+				if (server) {
+					const modifiedRequest = new Request(server.url + url.pathname, request);
+					return fetch(modifiedRequest).then(response => setCorsHeaders(response));
 				}
 			} else {
-				return new Response(JSON.stringify(matchedPath.response),{ headers: { 'Content-Type': 'application/json' }});
+				return setCorsHeaders(new Response(JSON.stringify(matchedPath.response), { headers: { 'Content-Type': 'application/json' } }));
 			}
 		}
 
-		return new Response(
+		// If no match found, but cors is enabled, return cors headers and 204
+		if (apiConfig.cors && request.method === 'OPTIONS') {
+			return setCorsHeaders(new Response(null, { status: 204 }));
+		}
+
+		return setCorsHeaders(new Response(
 			`No match found.`,
 			{ headers: { 'Content-Type': 'text/plain' } }
-		);
-	},
+		));
+	}
 };
