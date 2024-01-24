26d7ed1fcdda60f586ce0c01fbe2d7f240e23d93
İren SALTALI <iren@saltali.com>
2024-01-24 11:15:59 +0300

    Update API configuration and authentication handling

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -7,12 +7,7 @@ import { jwtAuth } from './auth';
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
 		// Handle JWT Authorization
-		if (apiConfig.authorizer && !await jwtAuth(request)) {
-			return setCorsHeaders(new Response(
-				`Unauthorized.`,
-				{ headers: { 'Content-Type': 'text/plain' }, status: 401 }
-			));
-		}
+		
 
 		const url = new URL(request.url);
 
@@ -28,6 +23,13 @@ export default {
 		const matchedPath = matchedPaths.find(item => item.method === request.method) || matchedPaths.find(item => item.method === 'ANY');
 
 		if (matchedPath) {
+			if (apiConfig.authorizer && matchedPath.auth && !await jwtAuth(request)) {
+				return setCorsHeaders(new Response(
+					`Unauthorized.`,
+					{ headers: { 'Content-Type': 'text/plain' }, status: 401 }
+				));
+			}
+
 			if (matchedPath.integration && matchedPath.integration.type === 'http_proxy') {
 				const server = apiConfig.servers.find(server => server.alias === matchedPath.integration.server);
 				if (server) {
