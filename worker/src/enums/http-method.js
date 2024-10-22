23ddce5f493a77face4295f2e9881e47cc9f48a5
gkocyigit <gokhanhkocyigit@gmail.com>
2024-10-22 16:08:24 +0200

    [feat] convert ts files to js files (#54)

---
+++ worker/src/enums/http-method.js
@@ -0,0 +1,21 @@
+const HttpMethod = {
+	GET : 'GET',
+	POST : 'POST',
+	PUT : 'PUT',
+	DELETE : 'DELETE',
+	OPTIONS : 'OPTIONS',
+	PATCH : 'PATCH',
+	ALL : '*',
+}
+
+const RequestMethod = {
+	GET : 'GET',
+	POST : 'POST',
+	PUT : 'PUT',
+	DELETE : 'DELETE',
+	OPTIONS : 'OPTIONS',
+	PATCH : 'PATCH',
+	ANY : 'ANY',
+}
+
+export { HttpMethod, RequestMethod };
