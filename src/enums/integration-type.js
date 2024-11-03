012e69f13c35ca6fa800693625f893c0351c9521
İren SALTALI <iren@saltali.com>
2024-11-03 23:26:49 +0300

    feat: Support for Workers Service Binding (#62)

--- src/enums/integration-type.js
+++ src/enums/integration-type.js
@@ -2,6 +2,7 @@ export const IntegrationTypeEnum = {
 	HTTP : 'http',
 	HTTP_PROXY : 'http_proxy',
 	SERVICE : 'service',
+	SERVICE_BINDING : 'service_binding',
 	AUTH0CALLBACK : 'auth0_callback',
 	AUTH0USERINFO : 'auth0_userinfo',
 	AUTH0CALLBACKREDIRECT : 'auth0_callback_redirect',
