bc4bd9d48461276acbeb90fb140c5e4e1c1e464f
İren SALTALI <iren@saltali.com>
2024-11-09 18:12:33 +0300

    fix: Add ServerlessAPIGatewayContext class for improved API context handling

--- src/types/serverless_api_gateway_context.js
+++ src/types/serverless_api_gateway_context.js
@@ -4,3 +4,15 @@
  * @property {URL} requestUrl - The request URL.
  * @property {JWTPayload} jwtPayload - The JWT payload.
  */
+export class ServerlessAPIGatewayContext {
+    /**
+     * @param {string} [apiConfig]
+     * @param {URL} [requestUrl]
+     * @param {JWTPayload} [jwtPayload]
+     */
+    constructor(apiConfig = null, requestUrl = null, jwtPayload = null) {
+        this.apiConfig = apiConfig;
+        this.requestUrl = requestUrl;
+        this.jwtPayload = jwtPayload;
+    }
+}
